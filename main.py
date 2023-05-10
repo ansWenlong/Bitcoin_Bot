5e435892ffe73fce0083be2ce0162199daa4be78
zhang2wl <longlong198@gmail.com>
2023-05-09 21:23:11 -0400

    for merge

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ price_data = pd.read_csv('price_data.csv')
 # add sma indicators
 # price_data = squeeze_momentum_indicator(price_data)
 # price_data = calculate_moving_averages(price_data,'close',5, 20)
-price_data = calculate_ema(price_data,'close',25)
-price_data = calculate_ema(price_data,'close',50)
+price_data = calculate_ema(price_data,'close',10)
+price_data = calculate_ema(price_data,'close',20)
 
-price_data = generate_signals(price_data, 'EMA25','EMA50')
+price_data = generate_signals(price_data, 'EMA10','EMA20')
 
 # save_price_data_to_csv(price_data, filename='price_data_with_indicator.csv')
 
 # plot the price data
 # plot_squeeze_momentum(price_data)
-plot_combined(price_data,'EMA25','EMA50')
+plot_combined(price_data,'EMA10','EMA20')
 
 # Backtesting the strategy
 start_day = '2022-11-16 05:00:00'
